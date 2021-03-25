0deae1e1fbef8210e528156272fc11ff7405804b
yun <yunqiangwu7@gmail.com>
2021-03-26 00:04:11 +0100

    page home

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+
+import './index.css'
+
+const Inicio = () => {
+    console.log('entro?');
+    return (
+        <>
+            <header className='header'>
+                <h1 className='h1'>Arrow Food</h1>
+            </header>
+        </>
+    )
+}
+export default Inicio
